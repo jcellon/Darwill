9d9f05bdda19e8c9809679701a9512f18d325946
Joseph <joecastman@gmail.com>
2021-05-21 10:24:10 -0500

    Added new change to environment

--- resources/views/darwill.blade.php
+++ resources/views/darwill.blade.php
@@ -12,8 +12,7 @@
         <li class="list-group-item">PHP 7.4.9</li>
         <li class="list-group-item">MySQL 8.0.21</li>
         <li class="list-group-item">Apache 2.4.46</li>
-        <li class="list-group-item">Data Store: PostgreSQL</li>
-        <li class="list-group-item">DB Admin: PostgreAdmin</li>
+        <li class="list-group-item">Data Admin: PostgreSQL</li>
         <li class="list-group-item">Host: Heroku</li>
     </ul>
 
